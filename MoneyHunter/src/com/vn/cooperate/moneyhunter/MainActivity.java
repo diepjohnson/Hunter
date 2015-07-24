81b4b8f346abb788331020670d89922f2b5c1b58
ducnm <ducnm@sohagame.vn>
2015-07-24 10:54:43 +0700

    ad code

--- MoneyHunter/src/com/vn/cooperate/moneyhunter/MainActivity.java
+++ MoneyHunter/src/com/vn/cooperate/moneyhunter/MainActivity.java
@@ -3,18 +3,61 @@ package com.vn.cooperate.moneyhunter;
 
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+import com.slidingmenu.lib.SlidingMenu;
 
 
 public class MainActivity extends FragmentActivity {
 
+	SlidingMenu slideMenu;
+	ImageView imgMenu;
+	Boolean isShowMenu = false;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-	
+		
+		slideMenu = (SlidingMenu) findViewById(R.id.sliding_menu);
+		imgMenu = (ImageView) findViewById(R.id.imgMenu);
+		imgMenu.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(!isShowMenu)
+				{
+					slideMenu.showBehind();
+					isShowMenu = true;
+				}
+				else
+				{
+					slideMenu.showAbove();
+					isShowMenu = false;
+				}
+			}
+		});
 	}
 
+	
+	Fragment curFragment;
+	public void changeFragment(Fragment fragment) {
+		try {
+			curFragment = fragment;
+			FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+			ft.replace(R.id.lnHomeContainer, fragment);
+			ft.commit();
+		} catch (Exception e) {
+			// TODO: handle exception
+			Log.e("ERR change frag ",""+ e.getMessage());
+		}
+		
+	}
 	
 }
---
+++ MoneyHunter/src/com/vn/cooperate/moneyhunter/fragment/ListAdAppFragment.java
@@ -0,0 +1,17 @@
+package com.vn.cooperate.moneyhunter.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListAdAppFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+}
