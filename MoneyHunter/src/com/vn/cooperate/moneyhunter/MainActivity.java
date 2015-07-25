d32bfdb55a7fc849bd2d4ce3870c31a597ccc3bb
Cu Van Diep <haclongnhi@gmail.com>
2015-07-25 09:07:49 +0700

    **

--- MoneyHunter/src/com/vn/cooperate/moneyhunter/MainActivity.java
+++ MoneyHunter/src/com/vn/cooperate/moneyhunter/MainActivity.java
@@ -1,63 +1,114 @@
 package com.vn.cooperate.moneyhunter;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.login.widget.LoginButton;
 import com.slidingmenu.lib.SlidingMenu;
 
+public class MainActivity extends FragmentActivity implements OnClickListener {
+	private SlidingMenu slideMenu;
+	private ImageView imgMenu;
+	private Boolean isShowMenu = false;
+	private RelativeLayout rlAvatar;
+	private LoginButton btnLogin;
+	private CallbackManager managerCallback;
+	
 
-public class MainActivity extends FragmentActivity {
-
-	SlidingMenu slideMenu;
-	ImageView imgMenu;
-	Boolean isShowMenu = false;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		slideMenu = (SlidingMenu) findViewById(R.id.sliding_menu);
-		imgMenu = (ImageView) findViewById(R.id.imgMenu);
-		imgMenu.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				if(!isShowMenu)
-				{
-					slideMenu.showBehind();
-					isShowMenu = true;
-				}
-				else
-				{
-					slideMenu.showAbove();
-					isShowMenu = false;
-				}
-			}
-		});
+		initUIControl();
+		getKeyHash();
 	}
 
-	
+	private void getKeyHash() {
+		PackageInfo info;
+		try {
+			info = getPackageManager().getPackageInfo(
+					"com.vn.cooperate.moneyhunter",
+					PackageManager.GET_SIGNATURES);
+			for (Signature signature : info.signatures) {
+				MessageDigest md;
+				md = MessageDigest.getInstance("SHA");
+				md.update(signature.toByteArray());
+				String something = new String(Base64.encode(md.digest(), 0));
+				// String something = new
+				// String(Base64.encodeBytes(md.digest()));
+				Log.e("hash key", something);
+			}
+		} catch (NameNotFoundException e1) {
+			Log.e("name not found", e1.toString());
+		} catch (NoSuchAlgorithmException e) {
+			Log.e("no such an algorithm", e.toString());
+		} catch (Exception e) {
+			Log.e("exception", e.toString());
+		}
+	}
+
+	private void initUIControl() {
+		slideMenu = (SlidingMenu) findViewById(R.id.sliding_menu);
+		imgMenu = (ImageView) findViewById(R.id.imgMenu);
+		imgMenu.setOnClickListener(this);
+		rlAvatar = (RelativeLayout) findViewById(R.id.rl_avatar);
+		rlAvatar.setOnClickListener(this);
+
+	}
+
 	Fragment curFragment;
+
 	public void changeFragment(Fragment fragment) {
 		try {
 			curFragment = fragment;
-			FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+			FragmentTransaction ft = getSupportFragmentManager()
+					.beginTransaction();
 			ft.replace(R.id.lnHomeContainer, fragment);
 			ft.commit();
 		} catch (Exception e) {
 			// TODO: handle exception
-			Log.e("ERR change frag ",""+ e.getMessage());
+			Log.e("ERR change frag ", "" + e.getMessage());
 		}
-		
+
 	}
-	
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.imgMenu:
+			if (!isShowMenu) {
+				slideMenu.showBehind();
+				isShowMenu = true;
+			} else {
+				slideMenu.showAbove();
+				isShowMenu = false;
+			}
+			break;
+		case R.id.rl_avatar:
+			Toast.makeText(MainActivity.this, "loging in facebook",
+					Toast.LENGTH_SHORT).show();
+			break;
+
+		default:
+			break;
+		}
+	}
+
 }
